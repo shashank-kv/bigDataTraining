98cc0dbcd468fe2f6525020ea9639385f187df71
Shashank KV <32491755+shashank-kv@users.noreply.github.com>
2021-09-24 09:12:15 +0530

    Add files via upload

---
+++ a1customer/src/dataframes.scala
@@ -0,0 +1,162 @@
+import java.util.Calendar
+
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object dataframes extends App{
+    val spark = SparkSession.builder()
+      .appName("Retail")
+      .master("local[2]")
+      .getOrCreate()
+  
+    val ordersDf = spark.read.csv("hdfs:///user/itv001177/project/orders").toDF("No", "Date", "CID", "Status")
+  
+    val customersDf = spark.read.csv("hdfs:///user/itv001177/project/customers").toDF("ID", "First", "Last", "PhoneNumber", "Email", "Address", "City", "State", "Pin")
+  
+    val combinedDf = customersDf.join(ordersDf, customersDf("ID") === ordersDf("CID"), "inner")
+  
+    //a. Get all records for customer based on name
+  
+    println("Enter first name")
+    val first = scala.io.StdIn.readLine()
+  
+    println("Enter second name")
+    val last = scala.io.StdIn.readLine()
+  
+    println("Get all records for customer based on name")
+  
+    val res1 = customersDf.where(s"First = '${first}' AND Last='${last}'")
+  
+    res1.show()
+  
+    //b. Count of orders based on Status and month
+  
+    println("Count of orders based on Status and month")
+    
+    val res3 = ordersDf.groupBy(month(col("Date")).as("month"),col("Status")).count()
+    
+    res3.show()
+    
+    //c. Count of orders based on Status and month for a customer
+  
+    println("Enter customer id for count of orders based on status and month")
+    
+    val cid = scala.io.StdIn.readLine()
+  
+    println("Count of orders based on Status and month for a customer")
+    
+    val res5 = ordersDf.where(s"CID=${cid}").groupBy(month(col("Date")).as("month"),col("Status")).count()
+    
+    res5.show()
+  
+    //d. Find count of orders based on customer and status
+  
+    println("Count of orders based on customer and status")
+  
+    val res6 = combinedDf.groupBy("CID", "State").count()
+  
+    res6.show()
+  
+    //e. Customers who placed orders
+  
+    println("Customers who placed orders")
+  
+    val res7 = combinedDf.select("ID", "First", "Last")
+  
+    res7.show()
+  
+    //f. Customers who have not placed orders
+  
+    println("Customers who have not placed orders")
+    
+    val leftDf = customersDf.join(ordersDf, customersDf("ID") === ordersDf("CID"), "left")
+    
+    val res8 = leftDf.where("CID = NULL")
+    
+    res8.show()
+  
+    //g. a. Top 5 highest number of orders
+  
+    println("Top 5 highest number of orders")
+  
+    val res9 = ordersDf.groupBy("CID").count().orderBy(col("count").desc).limit(5)
+  
+    res9.show()
+  
+    //g. b. Top 5 highest sum of total orders
+  
+    println("Top highest sum of total orders")
+  
+    val res10 = ordersDf.groupBy("CID").agg(sum("No").as("Total")).orderBy(col("Total").desc).limit(5)
+  
+    res10.show()
+  
+    //h. Customer who did not order in last 1 month or for long time
+  
+    println("Customer who did not order in last 1 month")
+  
+    val cur_month = Calendar.getInstance.get(Calendar.MONTH)
+  
+    val res12 = combinedDf.select("ID")
+      .where(s"${month(col("Date")).minus(cur_month + 1)} >= 1")
+  
+    res12.show()
+  
+    //i. Find last order date for all customers
+  
+    println("Last Order date for all customers")
+  
+    val res13 = ordersDf.groupBy("CID").agg(max("Date"))
+  
+    res13.show()
+  
+    //j. Find open and closed number of orders for each customer
+  
+    println("Number of closed orders")
+  
+    val res14 = combinedDf.where("Status == 'CLOSED' ").groupBy("ID").count()
+  
+    res14.show()
+  
+    println("Number of open orders")
+  
+    val res15 = combinedDf.where("Status != 'CLOSED' ").groupBy("ID").count()
+  
+    res15.show()
+  
+    //k. Find number of customers in every state
+  
+    println("Number of customers in every state")
+  
+    val res16 = customersDf.groupBy("State").count()
+  
+    res16.show()
+    
+    //l. Number of customers in every city
+    
+    println("Number of customers in every city")
+    
+    val res17 = customersDf.groupBy("City").count()
+  
+    res17.show()
+    
+    //m. Top 5 latest orders
+    
+    println("Top 5 latest orders")
+    
+    val res18 = ordersDf.orderBy(col("Date").desc).limit(5)
+  
+    res18.show()
+    
+    //n. Find count of orders based on city
+    
+    println("Orders in each city")
+    
+    val res19 = combinedDf.groupBy("City").count()
+    
+    res19.show()
+  
+    spark.stop()
+
+}
---
+++ a1customer/src/sql.scala
@@ -0,0 +1,132 @@
+import java.util.Calendar
+
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+
+object sql extends App{
+    val spark = SparkSession.builder()
+      .appName("Retail")
+      .master("local[2]")
+      .getOrCreate()
+  
+    val ordersDf = spark.read.csv("hdfs:///user/itv001177/project/orders").toDF("No", "Date", "CID", "Status")
+  
+    val customersDf = spark.read.csv("hdfs:///user/itv001177/project/customers").toDF("ID", "First", "Last", "PhoneNumber", "Email", "Address", "City", "State", "Pin")
+  
+    val combinedDf = customersDf.join(ordersDf, customersDf("ID") === ordersDf("CID"), "inner")
+  
+    ordersDf.createOrReplaceTempView("orders")
+  
+    customersDf.createOrReplaceTempView("customers")
+  
+    //a. Get all records for customer based on name
+  
+    println("Enter first name")
+    val first = scala.io.StdIn.readLine()
+  
+    println("Enter second name")
+    val last = scala.io.StdIn.readLine()
+  
+    println("Get all records for customer based on name")
+  
+    spark.sql(s"select * from customers where First='${first}' AND Last='${last}'").show()
+  
+    //b. Count of orders based on Status and month
+  
+    println("Count of orders based on Status and month")
+  
+    spark.sql("select count(No),Status,month(Date) from orders group by Status,month(Date)").show();
+  
+    //c. Count of orders based on Status and month for a customer
+  
+    println("Enter customer id for count of orders based on status and month")
+     
+    val cid = scala.io.StdIn.readLine()
+  
+    println("Count of orders based on Status and month for a customer")
+    
+    spark.sql(s"select count(No),Status,month(Date) from orders where CID=${cid} group by Status,month(Date)").show();
+  
+    //d. Find count of orders based on customer and status
+  
+    println("Count of orders based on customer and status")
+  
+    spark.sql("select count(No),customers.ID,Status from customers inner join orders where customers.ID=orders.CID group by customers.ID,Status").show()
+  
+    //e. Customers who placed orders
+  
+    println("Customers who placed orders")
+  
+    spark.sql("select First,Last,ID from customers inner join orders where customers.ID=orders.CID").show()
+  
+    //f. Customers who have not placed orders
+    
+    println("Customers who have not placed orders")
+    
+    spark.sql("select First,Last,ID from customers left join orders on customers.ID=orders.CID where orders.CID=NULL").show()
+     
+    //g. a. Top 5 highest number of orders
+    
+    println("Top 5 highest number of orders")
+    
+    spark.sql("select * from orders order by No desc limit 5").show();
+    
+    //g. b. Top 5 highest sum of total orders
+    
+    println("Top highest sum of total orders")
+    
+    spark.sql("select count(No), CID from orders group by CID order by count(No) desc limit 5").show();
+  
+    //h. Customer who did not order in last 1 month or for long time
+    
+    val cur_month = Calendar.getInstance.get(Calendar.MONTH)
+    
+    println("Customer who did not order in last 1 month")
+    
+    spark.sql("select CID FROM orders where (month(Date) - month(current_date)) >= 1").show()
+    
+    //i. Find last order date for all customers
+    
+    println("Last Order date for all customers")
+    
+    spark.sql("select max(Date) from orders group by CID").show()
+    
+    //j. Find open and closed number of orders for each customer
+    
+    println("Number of closed orders")
+    
+    spark.sql("select count(No),customers.ID from customers inner join orders where customers.ID=orders.CID AND Status='CLOSED' group by customers.ID").show()
+  
+    println("Number of open orders")
+    
+    spark.sql("select count(No),customers.ID from customers inner join orders where customers.ID=orders.CID AND Status!='CLOSED' group by customers.ID").show()
+    
+    //k. Find number of customers in every state
+    
+    println("Number of customers in every state")
+    
+    spark.sql("select count(ID),State from customers group by State").show()
+    
+    //l. Number of customers in every city
+    
+    println("Number of customers in every city")
+    
+    spark.sql("select count(ID),City from customers group by City").show()
+    
+    //m. Top 5 latest orders
+    
+    println("Top 5 latest orders")
+    
+    spark.sql("select * from orders order by Date desc limit 5").show();
+    
+    //n. Find count of orders based on city
+    
+    println("Orders in each city")
+    
+    spark.sql("select count(No), City from customers inner join orders where customers.ID=orders.CID group by City").show()
+    
+    spark.stop()
+    
+}
